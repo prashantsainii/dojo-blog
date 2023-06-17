c96856f74c7b516cf3c98691e4bc4bb42153d240
Prashant Saini <prashantsaini.code@gmail.com>
2023-06-17 21:54:56 +0530

    Till lec 20

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,21 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 const Home = () => {
-    return ( 
+    const { data:blogs, isPending, Error } = useFetch('http://localhost:8000/blogs');
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+    
+    return (
         <div className="home">
-            <h2>Homepage</h2>
+            { Error && <div>{error}</div> }
+            { isPending && <div>Loading...</div> }
+            { blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/> }
+
         </div>
     );
 }
- 
+
 export default Home;
